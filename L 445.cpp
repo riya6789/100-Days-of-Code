08501e761fdedb081bcc2f4b9ee6246f4c8fccd0
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-03-28 16:04:48 +0530

    Add Two Numbers II (M)

---
+++ L 445.cpp
@@ -0,0 +1,36 @@
+/* Can use stacks but then space increase, else can just reverse the linkedlists and add as done in a previous question
+*/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev = nullptr;
+        while(head){
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // reverse the input linked lists
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+
+        int carry = 0;
+        ListNode* ans = new ListNode();
+        ListNode* tail = ans;
+
+        while(l1 || l2 || carry){
+            int sum = carry;
+            if(l1) sum += l1->val, l1 = l1->next;
+            if(l2) sum += l2->val, l2 = l2->next;
+            carry = sum / 10;
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
+        }
+        ans = reverseList(ans->next);
+        return ans;
+    }
+};
