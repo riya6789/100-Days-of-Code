6d25dcfe77120f08ae261015e238079907412cb3
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-06-19 20:12:42 +0530

    Update L 2465.cpp

--- L 2465.cpp
+++ L 2465.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n=nums.size();
+        unordered_set<double> s;
+        int i=0; int j=n-1;
+        while(i<j){
+            double t=(double)(nums[i]+nums[j])/2.0;
+            s.insert(t);
+            i++;
+            j--;
+        }
+        return s.size();
+    }
+};
