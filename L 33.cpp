f6709d07258768dd67dcb43ce73922fc21f6364e
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-06-29 04:10:23 +0530

    L 33.cpp
    Search in Rotated Sorted Array(M)

---
+++ L 33.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& a, int t) {
+        int l=0;
+        int n=a.size();
+        int h=n-1;
+        while(h>=l){
+            int m=(l+h)/2;
+            if(a[m]==t)return m;
+            else if(a[l]<=a[m]){
+                if(a[l]<=t && t<=a[m])h=m-1;
+                else l=m+1;
+            }
+            else{
+                if(a[m]<=t && t<=a[h])l=m+1;
+                else h=m-1;
+            }
+        }
+        return -1;
+    }
+};
