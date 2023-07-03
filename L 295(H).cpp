7e5f08dc92e2bd91ce38ed8a2c7a81d9dae99b87
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-07-04 01:23:59 +0530

    Find Median from Data Stream(H)
    Priority queue, max and minheap

---
+++ L 295(H).cpp
@@ -0,0 +1,32 @@
+class MedianFinder {
+    priority_queue<int> maxHeap; // stores the first half of the elements (max heap)
+    priority_queue<int, vector<int>, greater<int>> minHeap; // stores the second half of the elements (min heap)
+
+public:
+    MedianFinder() {
+    }
+
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top())
+            maxHeap.push(num);
+        else
+            minHeap.push(num);
+
+        // Rebalance the heaps if necessary
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
+    }
+};
