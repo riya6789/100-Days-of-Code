b96cca4804a6545d76ba0e1a2861dbf8a8a202f5
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-07-04 17:58:10 +0530

    New easier method: Map and stack

--- L 496.cpp
+++ L 496.cpp
@@ -1,3 +1,25 @@
+//New my method -> store the nge in the map only for all elements of array b and finally retrieve nge from map for all elements of arr a
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& a, vector<int>& b) {
+        int n=b.size();
+        stack<int>s;
+        vector<int>v;
+        unordered_map<int,int>m;
+        m[b[n-1]]=-1;
+        s.push(b[n-1]);
+        for(int i=n-2; i>=0; i--){
+            while(!s.empty() && s.top()<=b[i]) s.pop();
+            m[b[i]]=s.empty()?-1:s.top();
+            s.push(b[i]);
+        }
+        for(int j:a){
+            v.push_back(m[j]);
+        }
+        return v;
+    }
+};
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
