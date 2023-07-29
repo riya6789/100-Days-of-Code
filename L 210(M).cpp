2c6a6db818c27198e3ad1866e8481204faf07911
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2023-07-29 06:22:47 +0530

    Course Scheduler II
    Topological sort bfs

---
+++ L 210(M).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& a) {
+        vector<vector<int>>adj(n);
+        for(auto it:a){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>in(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i])in[it]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(in[i]==0)q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int t=q.front();
+            q.pop();
+            ans.push_back(t);
+            for(auto it:adj[t]){
+                in[it]--;
+                if(in[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()==n)return ans;
+        return {};
+    }
+};
