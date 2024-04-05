a2ba5e3f0e4a186e03fdb9138872e93b8135a133
Riya Agarwal <98216277+riya6789@users.noreply.github.com>
2024-04-05 19:31:55 +0530

    Create CF Game with Integers 800.cpp

---
+++ CF Game with Integers 800.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3==0) {cout<<"SECOND"<<endl;}
+        else {cout<<"FIRST"<<endl;}
+    }
+    return 0;
+}
